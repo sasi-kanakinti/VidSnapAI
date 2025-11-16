fa78dcb311215330bfac1bd8ab4aa6e56300583d
Sasidhar <9908135565ks@gmail.com>
2025-11-16 04:34:13 +0000

    render deployment initial commit

--- main.py
+++ main.py
@@ -76,5 +76,5 @@ def start_worker():
 if __name__ == "__main__":
     start_worker()
 
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=False)
